d871520374fbf4b649c2c31703d7285ee2e1eba6
trong.hh <trong.hh@tctav.com>
2017-07-13 16:12:14 +0700

    upload

--- resources/views/admin/loaitin/edit.blade.php
+++ resources/views/admin/loaitin/edit.blade.php
@@ -1,11 +1,12 @@
-
+@extends('admin.layout.index')
+@section('content')
 <!-- Page Content -->
 <div id="page-wrapper">
   <div class="container-fluid">
     <div class="row">
       <div class="col-lg-12">
         <h1 class="page-header">Category
-          <small>Add</small>
+          <small>Edit</small>
         </h1>
       </div>
       <!-- /.col-lg-12 -->
@@ -43,7 +44,7 @@
               <input name="rdoStatus" value="2" type="radio">Invisible
             </label>
           </div>
-          <button type="submit" class="btn btn-default">Category Add</button>
+          <button type="submit" class="btn btn-default">Category Edit</button>
           <button type="reset" class="btn btn-default">Reset</button>
           <form>
       </div>
@@ -53,3 +54,5 @@
   <!-- /.container-fluid -->
 </div>
 <!-- /#page-wrapper -->
+
+  @endsection
